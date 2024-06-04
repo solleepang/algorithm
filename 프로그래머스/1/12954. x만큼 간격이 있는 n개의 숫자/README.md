dbbc810ad7d1f62c28c310fd188005d88792cb8c
solleepang <90190698+solleepang@users.noreply.github.com>
2024-06-04 15:51:22 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/README.md
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 1] x만큼 간격이 있는 n개의 숫자 - 12954 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12954) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12954#qna) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.04 ms
+메모리: 10.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 15:48:56
+2024년 06월 04일 15:51:20
 
 ### 문제 설명
 
--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -1,13 +1,13 @@
-def solution(x, n):
-
-    if x == 0:
-        return [0]*n
-    else:
-        return [i for i in range(x,(n*x-1 if x < 0 else n*x+1),x)]
-    
 # def solution(x, n):
+
 #     if x == 0:
-#         answer = [0]
+#         return [0]*n
 #     else:
-#         answer = list(range(x, (n+1)*x, x))
-#     return answer
+#         return [i for i in range(x,(n*x-1 if x < 0 else n*x+1),x)]
+    
+def solution(x, n):
+    if x == 0:
+        answer = [0]*n
+    else:
+        answer = list(range(x, (n+1)*x, x))
+    return answer
